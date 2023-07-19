6be48d63c5ae982ea4ba5f5b536487c41f59601f
Daniel <dwiest4@gmail.com>
2023-07-19 16:24:33 -0700

    Updated Contact Component.
     Changes to be committed:
    	modified:   src/components/common/Contact/Contact.tsx

--- src/components/common/Contact/Contact.tsx
+++ src/components/common/Contact/Contact.tsx
@@ -1,20 +1,63 @@
 const Contact = () => {
+  // return (
+  //   <section
+  //     id="contact"
+  //     className="section is-large has-background-danger-light"
+  //   >
+  //     <div className="container">
+  //       <div>CONTACT</div>
+  //       <p>
+  //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis velit
+  //         architecto cupiditate ratione ducimus perspiciatis totam labore
+  //         beatae, molestiae atque iste iusto, quae sunt dicta magni! Ducimus
+  //         amet, ipsum quasi perspiciatis doloremque nulla, voluptas distinctio
+  //         cupiditate voluptatibus tempora repellat. Et
+  //       </p>
+  //     </div>
+  //   </section>
+  // );
   return (
-    <section
-      id="contact"
-      className="section is-large has-background-danger-light"
-    >
-      <div className="container">
-        <div>CONTACT</div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis velit
-          architecto cupiditate ratione ducimus perspiciatis totam labore
-          beatae, molestiae atque iste iusto, quae sunt dicta magni! Ducimus
-          amet, ipsum quasi perspiciatis doloremque nulla, voluptas distinctio
-          cupiditate voluptatibus tempora repellat. Et
-        </p>
+    <footer className="footer">
+      <div className="columns">
+        <div className="column is-one-third is-flex is-justify-content-center is-align-content-center is-flex-direction-column">
+          <div className="subtitle is-flex is-justify-content-center">
+            Phone
+          </div>
+          <div className="is-flex is-justify-content-center">509.860.5434</div>
+        </div>
+        <div className="column is-one-third is-flex is-justify-content-center is-align-content-center is-flex-direction-column">
+          <div className="subtitle is-flex is-justify-content-center">
+            Email
+          </div>
+          <div className="is-flex is-justify-content-center">
+            dwiest4@gmail.com
+          </div>
+        </div>
+        <div className="column is-one-third is-flex is-justify-content-center is-align-content-center is-flex-direction-column">
+          <div className="subtitle is-flex is-justify-content-center">
+            Social
+          </div>
+          <div className="is-flex is-justify-content-center">
+            <span className="icon">
+              <a href="https://github.com/danieljwiest">
+                <img
+                  src="/img/favicons/icons8-github-64.png"
+                  alt="github icon"
+                ></img>
+              </a>
+            </span>
+            <span className="icon">
+              <a href="https://github.com/danieljwiest">
+                <img
+                  src="/img/favicons/icons8-linked-in-50.png"
+                  alt="linkedIn icon"
+                ></img>
+              </a>
+            </span>
+          </div>
+        </div>
       </div>
-    </section>
+    </footer>
   );
 };
 
