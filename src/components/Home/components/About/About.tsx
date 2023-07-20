20f93ad3ceb2604db73648633b191c4c57c870bf
Daniel <dwiest4@gmail.com>
2023-07-20 13:06:21 -0700

    Updated About Me description.
     Changes to be committed:
    	modified:   src/components/Home/components/About/About.tsx

--- src/components/Home/components/About/About.tsx
+++ src/components/Home/components/About/About.tsx
@@ -11,19 +11,31 @@ const About = () => {
           </div>
           <div className="tile is-parent is-vertical">
             <h1 className="title is-1">My Bio</h1>
-            <p className="">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis
-              velit architecto cupiditate ratione ducimus perspiciatis totam
-              labore beatae, molestiae atque iste iusto, quae sunt dicta magni!
-              Ducimus amet, ipsum quasi perspiciatis doloremque nulla, voluptas
-              distinctio cupiditate voluptatibus tempora repellat. Et
+            <p className="content">
+              I am a successful Mechanical Engineer with more than 11 years of
+              professional experience seeking a career change into software
+              development.
               <br />
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Recusandae, similique asperiores quia ducimus soluta praesentium?
-              Ratione nemo laboriosam optio accusantium! Similique dolorem
-              accusamus, modi maxime aperiam hic rerum dicta id molestiae
-              commodi blanditiis est suscipit quidem ratione deleniti dolores
-              natus.
+              <br />
+              In my previous career I was strong performer with experience in
+              both mangament and as a technical lead. My most recent roles
+              included:
+              <ul>
+                <li>
+                  Regional Team Manger for the Oregon Transmission Section
+                </li>
+                <li>Lead Technical Engineer</li>
+                <li>Project Manager</li>
+                <li>Program Technical Lead</li>
+              </ul>
+              <br />
+              To date my software experience has primarly utilized
+              Javascript/Typescript, React, and Node.JS. I am, however, an
+              Engineer at heart and am no stranger to adapting to the tools
+              needed for the project at hand.
+              <br />
+              <br />I am excited for the opportunity to continue to learn and
+              develop as Software Engineer.
             </p>
           </div>
         </div>
