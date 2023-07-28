facee2a0db8d9b8628056017df1c79fb0f02eca0
Daniel <dwiest4@gmail.com>
2023-07-28 13:32:23 -0700

    Updated Contact with correct link for linkedIn page.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   src/components/common/Contact/Contact.tsx

--- src/components/common/Contact/Contact.tsx
+++ src/components/common/Contact/Contact.tsx
@@ -1,21 +1,4 @@
 const Contact = () => {
-  // return (
-  //   <section
-  //     id="contact"
-  //     className="section is-large has-background-danger-light"
-  //   >
-  //     <div className="container">
-  //       <div>CONTACT</div>
-  //       <p>
-  //         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis velit
-  //         architecto cupiditate ratione ducimus perspiciatis totam labore
-  //         beatae, molestiae atque iste iusto, quae sunt dicta magni! Ducimus
-  //         amet, ipsum quasi perspiciatis doloremque nulla, voluptas distinctio
-  //         cupiditate voluptatibus tempora repellat. Et
-  //       </p>
-  //     </div>
-  //   </section>
-  // );
   return (
     <footer className="footer borderTop">
       <div className="columns">
@@ -47,7 +30,7 @@ const Contact = () => {
               </a>
             </span>
             <span className="icon">
-              <a href="https://github.com/danieljwiest">
+              <a href="https://www.linkedin.com/in/daniel-james-wiest/">
                 <img
                   src="/img/favicons/icons8-linked-in-50.png"
                   alt="linkedIn icon"
